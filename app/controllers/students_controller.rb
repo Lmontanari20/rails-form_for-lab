9011302fd63d50df89b7904b29d5eb32c481dca6
Lucas Montanari <Lmonty2011@gmail.com>
2020-12-14 20:53:57 -0800

    this was fun...

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,31 @@
+class StudentsController < ApplicationController
+    
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(set_student_params)
+        redirect_to student_path(@student)
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(set_student_params)
+        redirect_to student_path(@student)
+    end
+
+    private 
+
+    def set_student_params
+        params.require(:student).permit(:first_name, :last_name)
+    end
+end
--- spec/models/school_class_spec.rb
+++ spec/models/school_class_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe SchoolClass do
   before(:each) do
-    @school_class = SchoolClass.create!(title: "Computer Science", room_number: 5)
+    @schoolclass = SchoolClass.create!(title: "Computer Science", room_number: 5)
   end
 
   it 'can be created' do
-    expect(@school_class).to be_valid
+    expect(@schoolclass).to be_valid
   end
 
 end
